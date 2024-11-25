5dc228d6cab4c35bc646e7f03ac666e899ebfa9f
krishkad <krishkadam88@gmail.com>
2024-11-25 18:17:39 +0530

    trying lazy load with 2 calendar and suspense

--- src/components/shared/booking-box.tsx
+++ src/components/shared/booking-box.tsx
@@ -21,8 +21,8 @@ const BookingBox = () => {
                 className=" bg-white rounded-3xl md:rounded-full h-32 md:h-16 p-4 grid grid-cols-2 md:flex md:items-center md:justify-bwtween gap-5 border"
             >
                 <DatePicker />
-                {/* <DatePicker defaultDate={dayjs().month()} /> */}
-                <div className="w-full">date picker disable</div>
+                <DatePicker defaultDate={dayjs().month()} />
+                {/* <div className="w-full">date picker disable</div> */}
                 <Select>
                     <SelectTrigger className="w-full">
                         <SelectValue placeholder="2" />
