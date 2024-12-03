b51176e6e651fd9a6c5f00d4ab8a624970d6fa34
krishkad <krishkadam88@gmail.com>
2024-12-03 17:29:51 +0530

    gallery almost done only animation remaining

--- src/components/shared/gallery-component.tsx
+++ src/components/shared/gallery-component.tsx
@@ -1,5 +1,25 @@
+"use client";
 import Image from 'next/image';
-import React from 'react'
+import React from 'react';
+
+
+
+function splitArray<T>(array: Array<T>, numParts: number) {
+    const result: Array<Array<T>> = []
+
+    for (let i = 0; i < array.length; i++) {
+        const index = i % numParts
+        if (!result[index]) {
+            result[index] = []
+        }
+        result[index].push(array[i])
+    }
+
+    return result
+}
+
+
+
 
 const GalleryComponent = () => {
     const images = [
@@ -14,32 +34,54 @@ const GalleryComponent = () => {
         },
         {
             link: '/images/gallery/4.jpg'
-        }
+        },
+        {
+            link: '/images/gallery/5.jpg'
+        },
+        {
+            link: '/images/gallery/6.jpg'
+        },
+        {
+            link: '/images/gallery/7.jpg'
+        },
+        {
+            link: '/images/gallery/8.jpg'
+        },
+        {
+            link: '/images/gallery/9.jpg'
+        },
+        {
+            link: '/images/gallery/10.jpg'
+        },
+        {
+            link: '/images/gallery/11.jpg'
+        },
     ];
+
 
     return (
         <div className="relative w-full h-[550px] mt-14">
-            <div className="absolute top-0 inset-x-0 w-full h-20 bg-primary/25 z-10" />
-            <div className="absolute bottom-0 inset-x-0 w-full h-20 bg-primary/25 z-10" />
+            <div className="absolute -top-1 inset-x-0 w-full h-32 bg-gradient-top-bottom z-10" />
+            <div className="absolute -bottom-1 inset-x-0 w-full h-32 bg-gradient-bottom-top z-10" />
             <div className="size-full grid grid-cols-1 md:grid-cols-3 gap-5">
-                <div className="size-full bg-slate-100 space-y-6 overflow-y-scroll">
+                <div className="size-full space-y-10 overflow-y-scroll">
                     {images.map((li, i) => {
-                        return <div className="relative w-full h-[350px] p-3 rounded-md" key={i}>
-                            <Image src={li.link} fill sizes='100%' priority style={{ objectFit: 'cover' }} alt='images' />
+                        return <div className="relative w-full h-[400px] p-3 rounded-md" key={i}>
+                            <Image src={li.link} fill sizes='100%' priority style={{ objectFit: 'cover' }} className='rounded-xl' alt='images' />
                         </div>
                     })}
                 </div>
-                <div className="size-full bg-slate-100 space-y-6 overflow-y-scroll max-sm:hidden">
+                <div className="size-full space-y-10 overflow-y-scroll max-sm:hidden">
                     {images.map((li, i) => {
-                        return <div className="relative w-full h-[350px] p-3 rounded-md" key={i}>
-                            <Image src={li.link} fill sizes='100%' priority style={{ objectFit: 'cover' }} alt='images' />
+                        return <div className="relative w-full h-[400px] p-3 rounded-md" key={i}>
+                            <Image src={li.link} fill sizes='100%' priority style={{ objectFit: 'cover' }} className='rounded-xl' alt='images' />
                         </div>
                     })}
                 </div>
-                <div className="size-full bg-slate-100 space-y-6 overflow-y-scroll max-sm:hidden">
+                <div className="size-full space-y-10 overflow-y-scroll max-sm:hidden">
                     {images.map((li, i) => {
-                        return <div className="relative w-full h-[350px] p-3 rounded-md" key={i}>
-                            <Image src={li.link} fill sizes='100%' priority style={{ objectFit: 'cover' }} alt='images' />
+                        return <div className="relative w-full h-[400px] p-3 rounded-md" key={i}>
+                            <Image src={li.link} fill sizes='100%' priority style={{ objectFit: 'cover' }} className='rounded-xl' alt='images' />
                         </div>
                     })}
                 </div>
@@ -48,4 +90,4 @@ const GalleryComponent = () => {
     )
 }
 
-export default GalleryComponent
+export default GalleryComponent;
