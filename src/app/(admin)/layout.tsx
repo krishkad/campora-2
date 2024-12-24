116840c84d7c7f327ba0cc1e41ac5bdf5192cb47
krishkad <krishkadam88@gmail.com>
2024-12-24 18:35:47 +0530

    dashboard layout

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -2,25 +2,25 @@
 import Sidebar from '@/components/private/sidebar';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { AlignCenter } from 'lucide-react';
+import { AlignJustifyIcon } from 'lucide-react';
 import React, { ReactNode } from 'react'
 
 const AdminLayout = ({ children }: { children: ReactNode }) => {
 
     return (
         <div className="w-full flex">
-            <div className="w-[350px]">
+            <div className="hidden md:block w-[350px]">
                 <Sidebar />
             </div>
-            <main className="w-full bg-slate-100 p-5">
-                <div className="w-full flex items-center justify-between">
+            <main className="w-full min-h-svh h-full bg-gray-100 p-5">
+                <div className="max-w-6xl mx-auto flex items-center justify-between">
                     <div className="flex items-center justify-center gap-3">
                         <Button
                             size={'icon'}
                             variant={"outline"}
                             className='rounded-full'
                         >
-                            <AlignCenter className='w-5 h-5 inline' />
+                            <AlignJustifyIcon className='w-5 h-5 inline' />
                         </Button>
                         <h4 className="text-base font-semibold">
                             Dashboard
--- src/components/private/dashboard.tsx
+++ src/components/private/dashboard.tsx
@@ -1,9 +1,125 @@
-import React from 'react'
+import React from 'react';
+import {
+    BoxIcon,
+    IndianRupeeIcon,
+    SquareCheckIcon,
+    WalletIcon
+} from 'lucide-react';
 
 const Dashboard = () => {
     return (
-        <div className='border p-3 bg-white max-w-5xl mx-auto rounded-md'>
-            Dashboard
+        <div className=' max-w-6xl mx-auto rounded-md'>
+            <div className="w-full grid grid-cols-1 md:grid-cols-4 grid-rows-8 md:grid-rows-2 gap-3">
+                <div className="w-full aspect-[16/8] rounded-xl bg-white shadow-md border p-3 flex flex-col items-start justify-between">
+                    <div className="w-full flex items-start justify-between">
+                        <p className="font-semibold">Order</p>
+                        <BoxIcon className='w-7 h-7 shrink-0' />
+                    </div>
+                    <p className="w-full text-5xl font-semibold">
+                        201
+                    </p>
+                    <p className="text-xs font-medium">
+                        <span className="text-green-600">
+                            0.2%
+                        </span>{" "}
+                        Monthly Growth
+                    </p>
+                </div>
+                <div className="w-full aspect-[16/8] rounded-xl bg-white shadow-md border p-3 flex flex-col items-start justify-between">
+                    <div className="w-full flex items-start justify-between">
+                        <p className="font-semibold">Approved</p>
+                        <SquareCheckIcon className='w-7 h-7 shrink-0' />
+                    </div>
+                    <p className="w-full text-5xl font-semibold">
+                        201
+                    </p>
+                    <p className="text-xs font-medium">
+                        <span className="text-green-600">
+                            0.2%
+                        </span>{" "}
+                        Monthly Growth
+                    </p>
+                </div>
+                <div className="w-full h-full row-span-2 rounded-xl bg-white p-3 flex flex-col items-start justify-between border shadow-md">
+                    <div className="w-full flex items-start justify-between">
+                        <p className="font-semibold">Users</p>
+                        {/* <Users2Icon className='w-7 h-7 shrink-0' /> */}
+                    </div>
+                    <p className="w-full text-5xl font-semibold">
+                        201
+                    </p>
+                    <p className="text-xs font-medium">
+                        <span className="text-green-600">
+                            0.2%
+                        </span>{" "}
+                        Monthly Growth
+                    </p>
+                    <div className="w-full h-[75%]">
+                        graph
+                    </div>
+                </div>
+                <div className="w-full h-full row-span-2 rounded-xl bg-white p-3 flex flex-col items-start justify-between border shadow-md">
+                    <div className="w-full flex items-start justify-between">
+                        <p className="font-semibold">Subscriptions</p>
+                        {/* <Users2Icon className='w-7 h-7 shrink-0' /> */}
+                    </div>
+                    <p className="w-full text-5xl font-semibold">
+                        201
+                    </p>
+                    <p className="text-xs font-medium">
+                        <span className="text-green-600">
+                            0.2%
+                        </span>{" "}
+                        Monthly Growth
+                    </p>
+                    <div className="w-full h-[75%]">
+                        graph
+                    </div>
+                </div>
+                <div className="w-full aspect-[16/8] rounded-xl bg-white shadow-md border p-3 flex flex-col items-start justify-between">
+                    <div className="w-full flex items-start justify-between">
+                        <p className="font-semibold">Monthly Total</p>
+                        <IndianRupeeIcon className='w-7 h-7 shrink-0' />
+                    </div>
+                    <p className="w-full text-5xl font-semibold">
+                        35347
+                    </p>
+                    <p className="text-xs font-medium">
+                        <span className="text-green-600">
+                            0.2%
+                        </span>{" "}
+                        Monthly Growth
+                    </p>
+                </div>
+                <div className="w-full aspect-[16/8] rounded-xl bg-white shadow-md border p-3 flex flex-col items-start justify-between">
+                    <div className="w-full flex items-start justify-between">
+                        <p className="font-semibold">Revenue</p>
+                        <WalletIcon className='w-7 h-7 shrink-0' />
+                    </div>
+                    <p className="w-full text-5xl font-semibold">
+                        3534
+                    </p>
+                    <p className="text-xs font-medium">
+                        <span className="text-green-600">
+                            0.2%
+                        </span>{" "}
+                        Monthly Growth
+                    </p>
+                </div>
+            </div>
+            <div className="w-full grid grid-cols-1 md:grid-cols-2 grid-rows-2 md:grid-rows-1 mt-6 gap-3">
+                <div className="grid grid-cols-1 grid-rows-2 gap-3">
+                    <div className="w-full aspect-video rounded-xl shadow-md bg-white" />
+                    <div className="w-full aspect-video rounded-xl shadow-md bg-white" />
+                </div>
+                <div className="w-full grid grid-cols-1 grid-rows-8 gap-3">
+                    <div className="w-full h-full row-span-3 grid grid-cols-2 gap-2">
+                        <div className="w-full h-full rounded-xl shadow-md bg-white" />
+                        <div className="w-full h-full rounded-xl shadow-md bg-white" />
+                    </div>
+                    <div className="w-full h-full row-span-5 rounded-xl shadow-md bg-white" />
+                </div>
+            </div>
         </div>
     )
 }
