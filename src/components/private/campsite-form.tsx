917f41e2a5bd33646277b5c640306eb5dc17ef32
krishkad <krishkadam88@gmail.com>
2025-01-02 15:57:18 +0530

    campsite information form improved

--- src/components/private/campsite-form.tsx
+++ src/components/private/campsite-form.tsx
@@ -1,10 +1,12 @@
 "use client";
-import React from 'react'
+import React, { useState } from 'react'
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Form, FormControl, FormField, FormItem, FormLabel } from '../ui/form';
 import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+import { cn } from '@/lib/utils';
 
 
 const campsiteSchema = z.object({
@@ -23,7 +25,7 @@ const campsiteSchema = z.object({
 });
 
 const CampsiteForm = () => {
-
+    const [isDisabled, setIsDisabled] = useState<boolean>(true);
     const form = useForm<z.infer<typeof campsiteSchema>>({
         resolver: zodResolver(campsiteSchema),
         defaultValues: {
@@ -45,11 +47,15 @@ const CampsiteForm = () => {
     return (
         <div className="w-full">
             <Form {...form}>
-                <form onSubmit={form.handleSubmit(() => { })}>
+                <form
+                    onSubmit={form.handleSubmit(() => { })}
+                    className='w-full space-y-5'
+                >
                     <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-5">
                         <FormField
                             control={form.control}
                             name='campName'
+                            disabled={isDisabled}
                             render={({ field }) => {
                                 return <FormItem>
                                     <FormLabel>Camp Name</FormLabel>
@@ -63,6 +69,7 @@ const CampsiteForm = () => {
                         <FormField
                             control={form.control}
                             name='description'
+                            disabled={isDisabled}
                             render={({ field }) => {
                                 return <FormItem>
                                     <FormLabel>Description</FormLabel>
@@ -78,6 +85,7 @@ const CampsiteForm = () => {
                             <FormField
                                 control={form.control}
                                 name='totalCamps'
+                                disabled={isDisabled}
                                 render={({ field }) => {
                                     return <FormItem>
                                         <FormLabel>Total Camps</FormLabel>
@@ -86,6 +94,7 @@ const CampsiteForm = () => {
                                                 placeholder='Total Camps'
                                                 type='number'
                                                 value={field.value}
+                                                disabled={field.disabled}
                                                 onChange={field.onChange}
                                             />
                                         </FormControl>
@@ -96,6 +105,7 @@ const CampsiteForm = () => {
                             <FormField
                                 control={form.control}
                                 name='capacityPerCamp'
+                                disabled={isDisabled}
                                 render={({ field }) => {
                                     return <FormItem>
                                         <FormLabel>Capacity per Camp</FormLabel>
@@ -103,6 +113,7 @@ const CampsiteForm = () => {
                                             <Input
                                                 placeholder='Capacity'
                                                 type={'number'}
+                                                disabled={field.disabled}
                                                 value={field.value}
                                                 onChange={field.onChange}
                                             />
@@ -115,6 +126,7 @@ const CampsiteForm = () => {
                         <FormField
                             control={form.control}
                             name='pricing.costPerNightWithoutMeal'
+                            disabled={isDisabled}
                             render={({ field }) => {
                                 return <FormItem>
                                     <FormLabel>Camp Price Without Meal</FormLabel>
@@ -122,6 +134,7 @@ const CampsiteForm = () => {
                                         <Input
                                             type='number'
                                             placeholder='Price'
+                                            disabled={field.disabled}
                                             value={field.value}
                                             onChange={field.onChange}
                                         />
@@ -133,6 +146,7 @@ const CampsiteForm = () => {
                             <FormField
                                 control={form.control}
                                 name='pricing.mealCost.nonVeg'
+                                disabled={isDisabled}
                                 render={({ field }) => {
                                     return <FormItem>
                                         <FormLabel>Non-Veg Price</FormLabel>
@@ -140,6 +154,7 @@ const CampsiteForm = () => {
                                             <Input
                                                 placeholder='non-veg cost'
                                                 value={field.value}
+                                                disabled={field.disabled}
                                                 onChange={field.onChange}
                                                 type='number'
                                             />
@@ -150,6 +165,7 @@ const CampsiteForm = () => {
                             <FormField
                                 control={form.control}
                                 name='pricing.mealCost.veg'
+                                disabled={isDisabled}
                                 render={({ field }) => {
                                     return <FormItem>
                                         <FormLabel>Veg Price</FormLabel>
@@ -157,6 +173,7 @@ const CampsiteForm = () => {
                                             <Input
                                                 placeholder='veg cost'
                                                 value={field.value}
+                                                disabled={field.disabled}
                                                 onChange={field.onChange}
                                                 type='number'
                                             />
@@ -165,6 +182,26 @@ const CampsiteForm = () => {
                                 }}
                             />
                         </div>
+                    </div>
+                    <div className="w-full flex justify-between items-center">
+                        {isDisabled ? <div /> : <Button
+                            variant={"outline"}
+                            type='reset'
+                            onClick={() => {
+                                form.reset();
+                            }}
+                        >
+                            Reset
+                        </Button>
+                        }
+                        <Button
+                            variant={"outline"}
+                            className={cn('justify-self-end', !isDisabled && "bg-blue-600 text-white")}
+
+                            onClick={() => setIsDisabled(!isDisabled)}
+                        >
+                            {isDisabled ? "Edit" : "Save"}
+                        </Button>
                     </div>
                 </form>
             </Form>
