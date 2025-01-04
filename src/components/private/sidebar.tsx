8851ef525def6fccd262f9aa33ea1e651361c6a8
krishkad <krishkadam88@gmail.com>
2025-01-04 16:24:30 +0530

    mobile sidebar bar sheetclose

--- src/components/private/sidebar.tsx
+++ src/components/private/sidebar.tsx
@@ -16,28 +16,16 @@ import { ADMINROUTE } from "@/constants/index.c"
 import Link from 'next/link';
 import Logo from "../shared/logo";
 import { useEffect, useState } from "react";
-import { MOBILE_BREAKPOINT } from "@/hooks/use-mobile";
+import { MOBILE_BREAKPOINT, useIsMobile } from "@/hooks/use-mobile";
 import { SheetClose } from "../ui/sheet";
 
 
 
 export default function AppSidebar() {
-  
-    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
-    useEffect(() => {
-      const handleResize = () => {
-        setScreenWidth(window.innerWidth);
-      };
-  
-      window.addEventListener("resize", handleResize);
-  
-      // Cleanup the event listener on unmount
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }, []);
+    const isMobile = useIsMobile();
 
+   
     return (
         <Sidebar>
             <SidebarContent>
@@ -50,16 +38,14 @@ export default function AppSidebar() {
                             {ADMINROUTE.map((item, i) => (
                                 <SidebarMenuItem key={i} className="h-14 border-none">
                                     <SidebarMenuButton asChild className="h-full border-none focus:border-none focus-visible:ring-0">
-                                        {screenWidth >= MOBILE_BREAKPOINT ? (
-
-
+                                        {!isMobile ? (
                                             <Link href={item.href} className="size-full">
                                                 <item.icon className="w-5 h-6 " />
                                                 <span className="text-base">{item.label}</span>
                                             </Link>
                                         ) : (
-                                            <SheetClose asChild>
 
+                                            <SheetClose asChild>
                                                 <Link href={item.href} className="size-full">
                                                     <item.icon className="w-5 h-6 " />
                                                     <span className="text-base">{item.label}</span>
